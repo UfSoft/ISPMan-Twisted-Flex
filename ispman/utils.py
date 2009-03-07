29ee64537dcccdd79967aef0104f98d523db37a3
Pedro Algarvio <ufs@ufsoft.org>
2009-03-07 00:53:26 +0000

    Moved element style to the stylesheet.

---
+++ ispman/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8 et
+# ==============================================================================
+# Copyright © 2009 UfSoft.org - Pedro Algarvio <ufs@ufsoft.org>
+#
+# License: BSD - Please view the LICENSE file for additional information.
+# ==============================================================================
+
+class Hash(dict):
+    def param(self, key):
+        return self.get(key).encode('utf-8')
